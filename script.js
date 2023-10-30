507bf36e5327a4a8c8513158adf4b744760d3094
Megnan <105991363+mathisokaa@users.noreply.github.com>
2023-10-31 00:23:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function validerTexte() {
+    var texteSaisi = document.getElementById('userInput').value;
+    alert("Texte saisi : " + texteSaisi);
+    // Tu peux faire ici ce que tu veux avec le texte saisi
+    // Par exemple, l'envoyer à un serveur avec AJAX
+
+    // Pour l'exemple, affichons-le dans la console du navigateur
+    console.log("Texte saisi : " + texteSaisi);
+}
+
